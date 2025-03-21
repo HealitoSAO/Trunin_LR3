8ae4b65aadb6c63825f9c04b1be4ee7dad01b5d9
HealitoSAO <truninandre@yandex.ru>
2025-03-21 22:26:47 +0300

    number1

--- Trunin_LR3_project.cpp
+++ Trunin_LR3_project.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
 void inputNumbers(int &A, int &B, int &C) {
-   
+    std::cout << "Enter an integer A: ";
+    std::cin >> A;
+    std::cout << "Enter an integer B: ";
+    std::cin >> B;
+    std::cout << "Enter an integer C: ";
+    std::cin >> C;
 }
 
 void calculateRemainder(int A, int B, int C) {
