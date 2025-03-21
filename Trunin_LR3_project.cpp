6de5057b15175726c533912dcdace8d8c0d49a9f
HealitoSAO <truninandre@yandex.ru>
2025-03-21 22:25:46 +0300

    real main

--- Trunin_LR3_project.cpp
+++ Trunin_LR3_project.cpp
@@ -1,31 +1,26 @@
 #include <iostream>
 
-using namespace std;
+void inputNumbers(int &A, int &B, int &C) {
+   
+}
+
+void calculateRemainder(int A, int B, int C) {
+    
+}
+
+void calculateIntegerDivision(int A, int B, int C) {
+   
+}
+
+void processNumbers(int A, int B, int C) {
+   
+}
+
+void runProgram() {
+   
+}
+
 int main() {
-    // Ввод целых чисел A, B и C
-    int A, B, C;
-    
-    cout << "Enter an integer A: ";
-    cin >> A;
-    
-    cout << "Enter an integer B: ";
-    cin >> B;
-    
-    cout << "Enter an integer C: ";
-    cin >> C;
-
-    // Находим остаток от деления разности A и B на C
-    int remainder = (A - B) % C;
-    cout << "Remainder of (A - B) divided by C: " << remainder << endl;
-
-    // Находим целую часть от деления A на сумму B и C
-    int sum = B + C;
-    if (sum != 0) { // Проверка на деление на ноль
-        int integerPart = A / sum;
-        cout << "Integer part of A divided by (B + C): " << integerPart << endl;
-    } else {
-        cout << "Error: Division by zero!" << endl;
-    }
-
+    runProgram();
     return 0;
 }
